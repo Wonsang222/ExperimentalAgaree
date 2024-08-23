5a12c74538a925b4bb187b741c3b20e4690d459a
황원상 <wonsang222@naver.com>
2024-08-23 09:49:21 +0900

    Infra done

--- ExperimentalAgaree/ExperimentalAgaree/Infrastructure/Network/DataTransferService.swift
+++ ExperimentalAgaree/ExperimentalAgaree/Infrastructure/Network/DataTransferService.swift
@@ -19,7 +19,7 @@ protocol DataTransferDispatchQueue {
 }
 
 // 수정해야할수도있음. group
-#warning("Might 수정")
+#warning("Might 수정, group 추가해야할수도")
 extension DispatchQueue: DataTransferDispatchQueue {
     func asyncExecute(work: @escaping () -> Void) {
         async(execute: work)
@@ -42,3 +42,75 @@ protocol DataTransferService {
     ) -> NetworkCancellable? where F.Response == Void
 }
 
+final class DefaultDataTransferService {
+    private let networkService: NetworkService
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    private func decode<T:Decodable>(
+        data: Data?,
+        decoder: ResponseDecoder
+    ) -> Result<T, DatatransferError> {
+        do {
+            guard let data = data else { return .failure(.noResponse) }
+            let result: T = try decoder.decode(data)
+            return .success(result)
+        } catch {
+            return .failure(.parsing(error))
+        }
+    }
+}
+
+extension DefaultDataTransferService: DataTransferService {
+    func request<T, F>
+    (with endpoint: F,
+     on queue: DataTransferDispatchQueue,
+     completion: @escaping Completion<T>
+    ) -> NetworkCancellable? where T : Decodable, T == F.Response, F : ResponseRequestable {
+        let task = networkService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success(let data):
+                let result: Result<T, DatatransferError> = self.decode(data: data, decoder: endpoint.responseDecoder)
+                queue.asyncExecute {
+                    completion(result)
+                }
+            case .failure(let netErr):
+                queue.asyncExecute {
+                    completion(.failure(.networkFailure(netErr)))
+                }
+            }
+        }
+        return task
+    }
+    
+    func request<F>(
+        with endpoint: F,
+        on queue: DataTransferDispatchQueue,
+        completion: @escaping Completion<Void>
+    ) -> NetworkCancellable? where F : ResponseRequestable, F.Response == () {
+        let task = networkService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success:
+                queue.asyncExecute {
+                    completion(.success(()))
+                }
+            case .failure(let netErr):
+                queue.asyncExecute {
+                    completion(.failure(.networkFailure(netErr)))
+                }
+            }
+        }
+        return task
+    }
+}
+
+final class DefaultJsonResponseDecoder: ResponseDecoder {
+    let decoder = JSONDecoder()
+    func decode<T>(_ data: Data) throws -> T where T : Decodable {
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    
+}
